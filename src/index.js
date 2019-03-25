a94c94f1b0075c526b000665f9ccc664e161bbf9
adaltezze <adaltezze@gmail.com>
2019-03-26 01:33:05 +0500

    create Component App

--- src/index.js
+++ src/index.js
@@ -2,41 +2,48 @@ import './libs/fontawesome/css/all.css';
 import './libs/bootstrap/css/bootstrap.css';
 import './styles/styles.css';
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './components/Header';
 import Todo from './components/Todo';
 import todos from './data/todos.json';
 
-function App(props) {
-    return (
-        <div className="card shadow">
+class App extends Component {
 
-            <Header title={props.title} />
+    constructor(props) {
+        super(props);
+    }
 
-            <section className="card-body">
-                {props.todos.map(({ title, completed }, index) =>
-                    <Todo title={title} completed={completed} key={index} />
-                )}
-            </section>
+    render() {
+        return (
+            <div className="card shadow">
 
-            <footer className="card-body bg-dark">
+                <Header title={this.props.title} />
 
-                <form className="row">
-                    <div className="col">
-                        <input className="form-control h-100" type="text" placeholder="Заголовок задачи" />
-                    </div>
-                    <div className="col-auto">
-                        <button className="btn btn-light" type="submit">
-                            Добавить
-                        </button>
-                    </div>
-                </form>
+                <section className="card-body">
+                    {this.props.todos.map(({ title, completed }, index) =>
+                        <Todo title={title} completed={completed} key={index} />
+                    )}
+                </section>
 
-            </footer>
-        </div>
-    );
+                <footer className="card-body bg-dark">
+
+                    <form className="row">
+                        <div className="col">
+                            <input className="form-control h-100" type="text" placeholder="Заголовок задачи" />
+                        </div>
+                        <div className="col-auto">
+                            <button className="btn btn-light" type="submit">
+                                Добавить
+                            </button>
+                        </div>
+                    </form>
+
+                </footer>
+            </div>
+        );
+    }
 }
 
 App.propTypes = {
